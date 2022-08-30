afcac4a38071e945101ad8eb9116c8190e5b5e53
nguyenduccanh011 <112483234+nguyenduccanh011@users.noreply.github.com>
2022-08-30 19:43:51 +0700

    Add files via upload

---
+++ cartegoryadd.php
@@ -0,0 +1,20 @@
+<?php
+include "header.php";
+include "slider.php";
+
+?>
+
+<div class="admin-content-right">
+            <div class="admin-content-right-cartegory_add">
+                <h1>
+                    Thêm danh mục
+                </h1>
+                <form action="" method="post">
+                    <input type="text" placeholder="Nhập tên danh mục">
+                    <button type="submit">Thêm</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
